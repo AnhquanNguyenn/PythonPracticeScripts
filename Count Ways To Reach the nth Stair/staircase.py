00b55a4cba905186bd4de60e28de7dc3baaac381
Anhquan Nguyen <anhquan.nguyen5417@gmail.com>
2019-08-10 21:19:34 -0500

    Updated O(n) solution

--- Count Ways To Reach the nth Stair/staircase.py
+++ Count Ways To Reach the nth Stair/staircase.py
@@ -21,3 +21,28 @@ if n == 2, (1, 1) (2)	2 ways
 if n == 3, (1, 1, 1) (1, 2) (2, 1) 3 ways 
 if n == 4, (1, 1, 1, 1) (1, 2, 1) (1, 1, 2) (2, 1, 1) (2, 2)	5 ways
 '''
+
+# Recursive function used by countWays
+
+
+def countWaysUtil(n, m):
+    res = [0 for x in range(n)]  # Creates list res with all elements 0
+    res[0], res[1] = 1, 1
+
+    for i in range(2, n):
+        j = 1
+        while j <= m and j <= i:
+            res[i] = res[i] + res[i - j]
+            j = j + 1
+    return res[n - 1]
+
+# Returns number of ways to reach s'th stair
+
+
+def countWays(s, m):
+    return countWaysUtil(s + 1, m)
+
+
+# Driver Program
+s, m = 5, 2
+print("Number of ways =", countWays(s, m))
