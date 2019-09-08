c5c6523bc88321c769ad9e8e7d23f5ed43fbd6d2
Anhquan Nguyen <anhquan.nguyen5417@gmail.com>
2019-09-08 00:15:54 -0500

    Updated solution

--- Maximum Values Within Each Subarray/maxValues.py
+++ Maximum Values Within Each Subarray/maxValues.py
@@ -39,3 +39,41 @@ def maxValues(numbers, k):
 print(maxValues([10, 5, 2, 7, 8 ,7], 3))
 print(maxValues([10, 5, 2, 7, 8 ,7], 2))
 print(maxValues([10, 5, 2, 7, 8, 7], 4))
+
+'''
+from collections import deque
+
+
+def get_sliding_max(a, k):
+
+    window_max_elements = list()
+
+    if not a:
+        return None
+    if len(a) <= k:
+        return max(a)
+
+    dq = deque()
+
+    for i in range(k):
+        while dq and a[dq[-1]] < a[i]:
+            dq.pop()
+        dq.append(i)
+    window_max_elements.append(a[dq[0]])
+
+    for i in range(k, len(a)):
+        while dq and dq[0] <= i - k:
+            dq.popleft()
+
+        while dq and a[dq[-1]] < a[i]:
+            dq.pop()
+        dq.append(i)
+
+        window_max_elements.append(a[dq[0]])
+
+    return window_max_elements
+
+
+assert get_sliding_max([10, 5, 2, 7, 8, 7], 3) == [10, 7, 8, 8]
+assert get_sliding_max([5, 2, 1], 2) == [5, 2]
+'''
