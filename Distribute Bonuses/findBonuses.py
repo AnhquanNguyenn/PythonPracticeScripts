c8794b22e8f597920c8bd242300ff1e017ed4926
Anhquan Nguyen <anhquan.nguyen5417@gmail.com>
2019-10-01 13:36:17 -0500

    List Analysis

--- Distribute Bonuses/findBonuses.py
+++ Distribute Bonuses/findBonuses.py
@@ -13,20 +13,39 @@ def bonus(numbers):
         if index < 1:
             # can only be a +1 or + 2, since they can only be higher than one other person
             if numbers[index] < numbers[index + 1]:
-                bonusList.append(1)
-                index += 1
+                bonusList.append(1)   
             else:
                 bonusList.append(2)
-                index += 1
-        # for the cases where all the employees will have a left and right person to compare to
-        elif index >= 1 and index < len(numbers):
-            index += 1
-        elif index >= len(numbers):
             
-                    
-        
-                
-        
+            index += 1
+            
+        # for the cases where all the employees will have a left and right person to compare to
+        elif index >= 1 and index < (len(numbers) - 1):
+            toTheLeft = index - 1
+            toTheRight = index + 1
+            
+            if numbers[index] > numbers[toTheLeft]:
+                if numbers[index] > numbers[toTheRight]:
+                    bonusList.append(3)
+                else:
+                    bonusList.append(2)
+            else:
+                if numbers[index] > numbers[toTheRight]:
+                    bonusList.append(2)
+                else:
+                    bonusList.append(1)
+                     
+            index += 1
+            
+        # for the case of the last number in the list where there is no right person to compare to. 
+        elif index == len(numbers) - 1:
+            toTheLeft = index - 1
+            
+            if numbers[index] > numbers[toTheLeft]:
+                bonusList.append(2)
+            else:
+                bonusList.append(1)
+            
     return bonusList
 
 
