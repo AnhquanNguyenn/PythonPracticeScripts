e4cf7017de5cf81c8e144e06f1c1b6cb867df542
Anhquan Nguyen <anhquan.nguyen5417@gmail.com>
2019-10-11 14:01:20 -0500

    Tree Anaylsis

--- Symmetric K-ary Tree/symmetric.py
+++ Symmetric K-ary Tree/symmetric.py
@@ -19,8 +19,6 @@ def isMirror(root1, root2):
         if root1.value == root2.value:
             return (isMirror(root1.left, root2.right) and isMirror(root1.right, root2.left))
     
-    
-    
     return False
 
 
